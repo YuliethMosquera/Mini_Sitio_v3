9565a95d6dfd72daf6fa29c43421fd209def3360
STEFANY YULIETH MOSQUERA PALACIOS <stefanymosquera72@gmail.com>
2025-11-18 10:45:38 -0500

    feat(matematicas): agregamos variables, las agregamos para hacer mas tarde la funcion de operaciones (#5)

---
+++ matematicas.js
@@ -0,0 +1,5 @@
+let num1 = 25;
+let num2 = -10;
+let raiz_cuadrada = Math.sqrt(num1); // La variable contendrá 5
+let valor_absoluto = Math.abs(num2); // La variable contendrá 10
+let potencia = Math.pow(2, 3); // La variable contendrá 8
